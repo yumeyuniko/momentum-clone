57610ad5a0cc7e378e88aa8a1ceb045939d256dc
yumeyuniko <daigocafe@msn.com>
2022-04-04 20:30:23 +0900

    date

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Weather from './components/Weather';
 import React from 'react';
 import Time from './components/Time';
+import Date from './components/Date';
 
 function App() {
   return (
     <div>
       <Weather />
       <Time />
+      <Date />
     </div>
   );
 }
---
+++ src/components/Date.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DateToday = () => {
+  let today = new Date();
+  // console.log(today);
+  let date =
+    today.getFullYear() + '/' + today.getMonth() + '/' + today.getDate();
+
+  return (
+    <div>
+      <p>{date}</p>
+    </div>
+  );
+};
+
+export default DateToday;
